3fba152bde8c7cd3c23c571f3eea43d09f833f4a
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-09-09 20:58:29 -0300

    fix: port number

--- index.js
+++ index.js
@@ -48,6 +48,9 @@ app.get(`/produtos`, (req, res) => {
   res.json(filtro);
 });
 
-app.listen(3000, () => {
-  console.log(`The server is up and running on port 3000`);
+const port = process.env.PORT | 3000;
+
+app.listen(port, () => {
+  console.log(`The server is up and running on port ${port}`);
 });
+
